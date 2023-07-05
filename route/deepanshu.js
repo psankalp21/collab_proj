ca8155594a6a7406bf2c2f61e193416009e632e6
Deepanshu-Appinvent <deepanshu.gupta@appinventiv.com>
2023-07-05 15:48:23 +0530

    deepanshu feature branch

--- route/deepanshu.js
+++ route/deepanshu.js
@@ -1,1 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/multiply", (req, res) => {
+  try {
+    const { num1, num2, operator } = req.body;
+    const result = Number(num1) * Number(num2);
+    res.json({ num1, operator, num2, result });
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while performing multiplication." });
+  }
+});
+
+router.post("/divide", (req, res) => {
+  try {
+    const { num1, num2, operator } = req.body;
+    const result = Number(num1) / Number(num2);
+    res.json({ num1, operator, num2, result });
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while performing division." });
+    console.log("Error ")
+  }
+});
+
+module.exports = router;
 
