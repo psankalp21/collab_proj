451f16c49038b6704edb6891f9eddfddf4e7fb21
psankalp21 <prem.sankalp@appinventiv.com>
2023-07-05 13:38:45 +0530

    index modified

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 require('dotenv').config();
+
+const bodyParser = require('body-parser');
+
+const abhijeetRoute = require('./route/abhijeet')
+const deepanshuRoute = require('./route/deepanshu')
+const sankalpRoute = require('./route/sankalp')
+
 const app = express();
 
 const port = process.env.PORT;
-app.get('/',(req,res)=>{
-    res.send("Welcome to home page")
-})
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use(abhijeetRoute);
+app.use(deepanshuRoute);
+app.use(sankalpRoute);
+
 app.listen(port,() =>{
-    console.log(`running on ${port}`)
+    console.log("running")
 })
+
